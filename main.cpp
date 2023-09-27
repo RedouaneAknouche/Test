40af56866fa4e694515e888446609f3557c95319
redouane aknouche <redouaneaknouche@gmail.com>
2023-09-27 17:33:51 -0400

    Added Features

--- main.cpp
+++ main.cpp
@@ -7,5 +7,7 @@ int main(){
 
 
     std::cout << "Hello, Git" << std::endl;
+    std::cout << "New Features --Warning" << std::endl;
+    std::cout << "New Features --Danger"  << std::endl;
     
 }
