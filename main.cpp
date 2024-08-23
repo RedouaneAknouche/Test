7c257f3d0366f330df82d307d9060281db0a2219
Redouane Aknouche <RedouaneAknouche@gmail.com>
2024-08-23 09:16:39 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,1 @@
-#include <iostream>
 
-
-
-
-int main(){
-
-
-    std::cout << "Hello, Git" << std::endl;
-    std::cout << "New Features --Warning" << std::endl;
-    std::cout << "New Features --Danger"  << std::endl;
-    
-}
